c6184376abcef39760979fb7b12468b51d16835e
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2023-03-14 14:50:49 -0700

    second commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :users
+  resources :sessions
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get 'pages/home'
+  get 'pages/live_draft'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+end
